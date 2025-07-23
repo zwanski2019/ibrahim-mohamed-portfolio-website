d2ccc72e6903712c737c5dee802696b3a51fbe3e
Mohamed Zwanski <53589819+zwanski2019@users.noreply.github.com>
2025-07-23 21:43:01 +0100

    Read Supabase credentials from env

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,10 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ceihcnfngpmrtqunhaey.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlaWhjbmZuZ3BtcnRxdW5oYWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxMTIwNzMsImV4cCI6MjA2NTY4ODA3M30.I2Yit_peN5PiTq54Y-4hrDowH3wEWa7lZPT0UgKdXSc";
+const SUPABASE_URL = import.meta.env.SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY as string;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
