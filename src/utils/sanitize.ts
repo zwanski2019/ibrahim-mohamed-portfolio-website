c93da8c1fd59c0960a0e00e0995e8b81fe04d96e
Mohamed Zwanski <53589819+zwanski2019@users.noreply.github.com>
2025-07-28 02:09:07 +0100

    fix: load jsdom safely

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -7,7 +7,7 @@ let windowRef: Window;
 
 if (typeof window === 'undefined') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { JSDOM } = require('jsdom');
+  const { JSDOM } = eval('require')("jsdom");
   windowRef = new JSDOM('').window as unknown as Window;
 } else {
   windowRef = window;
